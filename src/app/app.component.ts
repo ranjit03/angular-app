08872227b66247d8b6289f172ce899800d281df4
ranjit <ranjitendurthi03@gmail.com>
2019-06-25 09:47:58 +1200

    passing events between components

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular1';
+  rootName = 'Test';
+  onNameChanged(newName){
+    this.rootName = newName;
+  }
 }
--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -1,16 +1,20 @@
-import {Component} from '@angular/core';
+import {Component, Input, Output, EventEmitter} from '@angular/core';
 
 @Component({
   selector: 'app-user',
   template: `<p>Hello {{name}}</p>
   <p>This is user</p>
-  <input type ="text" (input) = "onUserInput($event)" [value] = "name">`
+<!-- <input type ="text" [(ngModel)]="name">-->
+<input type = "text" (input)="onUserInput($event)" [value]="name" >
+`
 })
 
 
 export class UserComponent {
-  name = 'Ranjit';
-  onUserInput(event){
-    this.name = event.target.value;
+  @Input() name ;
+  @Output () nameChanged = new EventEmitter<string>();
+  onUserInput(event) {
+  //   this.name = event.target.value;
+  this.nameChanged.emit(event.target.value);
   }
 }
