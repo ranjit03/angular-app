649da16733a2d742bb09e99e7b3ddee133530f2e
ranjit <ranjitendurthi03@gmail.com>
2019-06-25 14:57:01 +1200

    added user-detail component

--- src/app/user.component.ts
+++ src/app/user.component.ts
@@ -6,7 +6,7 @@ import {Component, Input, Output, EventEmitter} from '@angular/core';
   <p>This is user</p>
 <!-- <input type ="text" [(ngModel)]="name">-->
 <input type = "text" (input)="onUserInput($event)" [value]="name" >
-`
+<app-user-detail></app-user-detail>`
 })
 
 
